4ced3bffd68437cb397fbb465ded33c6bcc195e2
shashank <shashankraturi4@gmail.com>
2021-04-19 19:49:36 +0530

    Added Upate and Delete Request

--- src/main/java/org/DemoApp/topic/TopicController.java
+++ src/main/java/org/DemoApp/topic/TopicController.java
@@ -1,9 +1,7 @@
 package org.DemoApp.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,12 +13,27 @@ public class TopicController {
     private TopicService topicService;
 
     @RequestMapping("/topics")
-    public List<Topic> getAllTopics(){
+    public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
     @RequestMapping("/topics/{id}")
-    public Topic getTopic(@PathVariable String id){
+    public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(value = "/topics", method = RequestMethod.POST)
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(value = "/topics/{id}", method = RequestMethod.PUT)
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(topic, id);
+    }
+
+    @RequestMapping(value = "/topics/{id}", method = RequestMethod.DELETE)
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
     }
 }
--- src/main/java/org/DemoApp/topic/TopicService.java
+++ src/main/java/org/DemoApp/topic/TopicService.java
@@ -2,22 +2,42 @@ package org.DemoApp.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics =  Arrays.asList(
+    private List<Topic> topics =  new ArrayList<>(Arrays.asList(
             new Topic("Spring" , "Spring Framework" , "Spring Framework desc"),
             new Topic("Python" , "Python Framework" , "Python Framework desc"),
             new Topic("JS" , "JS Framework" , "JS Framework desc")
-    );
+    ));
 
     public List<Topic> getAllTopics(){
         return topics;
     }
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+
+        for(int i = 0; i < topics.size(); ++i){
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i , topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
